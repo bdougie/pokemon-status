edb59561feedb09f7095feada33af4ae8273ea72
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-05-28 04:17:16 -0700

    remove cloudfront url

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,14 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    SRI: {
-      crossorigin: 'anonymous'
-    },
-    fingerprint: {
-      prepend: 'https://subdomain.cloudfront.net/'
-    }
-  });
+  var app = new EmberApp(defaults, {});
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
