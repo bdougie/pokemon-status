28b751d27fb523ad68647b7ceae51f2920a0bead
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-05-28 04:20:04 -0700

    add SRI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {});
+  var app = new EmberApp(defaults, {
+    SRI: {
+      crossorigin: 'anonymous'
+    },
+  });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
